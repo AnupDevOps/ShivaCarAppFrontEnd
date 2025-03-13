8f373414fe7be9aea140fda0e6af8bcf6063f6c8
Anup Kumar Mishra <46366695+AnupDevOps@users.noreply.github.com>
2025-03-13 22:31:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,12 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
   <BrowserRouter basename="/ShivaCarAppFrontEnd">
     <App />
-  </BrowserRouter>
-);
+  </BrowserRouter>;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
